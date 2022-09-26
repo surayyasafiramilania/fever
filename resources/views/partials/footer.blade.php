bd0ebf058024629ab5991f8a0bb999d15fe6c35a
Surayya Safira Milania <vsfram18@gmail.com>
2022-09-26 19:15:13 +0700

    update tahun

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
     <div class="copyright">
-        <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quixlab</a> 2018</p>
+        <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quixlab</a> 2022</p>
     </div>
